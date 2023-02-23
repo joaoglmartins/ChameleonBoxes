58c534e7780462aafb8c46505ce2664420821c47
joaoglmartins <93201286+joaoglmartins@users.noreply.github.com>
2023-02-23 12:43:35 +0000

    Changes colors as you click

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -2,12 +2,19 @@ import { useState } from "react";
 import "./Box.css";
 
 export default function Box(props) {
-  const [color, setColor] = useState("vermelho");
+  const [color, setColor] = useState("#fffff");
 
   const toggleColor = () => {
-    setColor((prevColor) => {
-      return prevColor === "vermelho" ? "azul" : "vermelho";
+    setColor(() => {
+      let randomColor = Math.floor(Math.random() * 16777215).toString(16);
+      return "#" + randomColor;
     });
   };
-  return <div className={`Box ${color}`} onClick={toggleColor}></div>;
+  return (
+    <div
+      className='Box'
+      style={{ backgroundColor: color }}
+      onClick={toggleColor}
+    ></div>
+  );
 }
